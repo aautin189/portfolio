b401d2c14cf87a99ea0a7f2e23b2dd75c1b9daf3
anautin <anautin@uno.edu>
2020-03-10 13:51:12 -0700

    refactored form

--- scripts/interactive.js
+++ scripts/interactive.js
@@ -2,11 +2,16 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
 
+
+// assuming there are multiple accordions
 for (i = 0; i < acc.length; i++) {
   
-
+	// add an event listener for each
   acc[i].addEventListener("click", function() {
+    
+  	// used for true or false (the inverse) - active is revealed 
     this.classList.toggle("active");
+    //iterate through each element nested inside the accordion
     var panel = this.nextElementSibling;
     
     if (panel.style.maxHeight) {
